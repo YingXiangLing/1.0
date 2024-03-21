585e719a523fcf7f918ca71e970d681bcb7b9382
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-03-21 14:10:15 +0200

    Update and rename e.lua to Invis.lua

---
+++ Invis.lua
@@ -0,0 +1,10 @@
+local player = game.Players.LocalPlayer
+position     = player.Character.HumanoidRootPart.Position
+wait(0.1)
+player.Character:MoveTo(position + Vector3.new(0, 1000000, 0))
+wait(0.1)
+humanoidrootpart = player.Character.HumanoidRootPart:clone()
+wait(0.1)
+player.Character.HumanoidRootPart:Destroy()
+humanoidrootpart.Parent = player.Character
+player.Character:MoveTo(position)
