b159e5ef5b945a07d0ae1fc1a0c86efb0e0cc234
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-04-20 15:00:35 +0300

    Update FlingThing.lua

--- FlingThing.lua
+++ FlingThing.lua
@@ -37,3 +37,40 @@ tool.Activated:Connect(function()
 		end
 		end
 end)
+
+local targete = nil
+local toole = Instance.new("Tool",game.Players.LocalPlayer.Backpack)
+toole.Name = "Part Deleter"
+toole.RequiresHandle = false
+toole.CanBeDropped = false
+toole.ToolTip = "Delete unanchored parts"
+toole.Activated:Connect(function()
+    if game.Players.LocalPlayer:GetMouse().Target ~= nil then
+        if targete == nil then
+            if not game.Players.LocalPlayer:GetMouse().Target.Parent:FindFirstChildOfClass("Humanoid") and game.Players.LocalPlayer:GetMouse().Target.Anchored == false then
+                local e = Instance.new("Message",workspace)
+                game.Debris:AddItem(e,2)
+                e.Text = "Remember! you need to have the networkclient ownership of the part you are trying to delete, try to touch it a few times or stand on it if it dosent work."
+                local sb = Instance.new('SelectionBox',game.Players.LocalPlayer:GetMouse().Target)
+                sb.Adornee = game.Players.LocalPlayer:GetMouse().Target
+                sb.Name = "TeleportingBox"
+                targete = game.Players.LocalPlayer:GetMouse().Target
+            pcall(function()
+                local oldp
+                local oldm 
+                    oldm = game.Players.LocalPlayer:GetMouse().Hit.Position
+                oldp = game.Players.LocalPlayer.Character:GetPivot()
+                task.wait()
+                    game:GetService("RunService").Heartbeat:Wait()
+                    game.Players.LocalPlayer.Character:PivotTo(CFrame.new(targete.Position+Vector3.new(0,-1,0)))
+                wait(0.6)
+                game.Players.LocalPlayer.Character:PivotTo(oldp)
+                targete.Position =  Vector3.new(0,-70,0)
+                targete:FindFirstChild("TeleportingBox"):Destroy()
+                wait(0.1)
+                targete = nil
+            end)
+        end
+        end
+end)
+
