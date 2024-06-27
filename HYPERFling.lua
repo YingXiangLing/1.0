9c1a15029f9ead3026c484edd61240b91c070ff3
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-06-27 20:38:20 +0300

    Update HYPERFling.lua

--- HYPERFling.lua
+++ HYPERFling.lua
@@ -8,6 +8,7 @@ local CanUse = true
 local function Fling(TargetName)
 	if Players:FindFirstChild(TargetName) then
 		local HRP = plr.Character:FindFirstChild("HumanoidRootPart")
+		HRP:FindFirstChildOfClass("Motor6D").Enabled = false
 		local Target = Players:FindFirstChild(TargetName).Character
 		workspace.CurrentCamera.CameraSubject = Target:FindFirstChild("Head")
 		HRP.CFrame = Target:GetPivot()
