618dd9d88588b147272d84ff94216155e7921039
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-06-30 23:55:44 +0300

    Update HYPERFling.lua

--- HYPERFling.lua
+++ HYPERFling.lua
@@ -99,7 +99,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(R)
 						if workspace:FindFirstChild(v.Name) and  (oldpos.Position-v.Character:FindFirstChild("HumanoidRootPart").Position).Magnitude <= 20  then
 							Fling(v.Name)
 							Success = true
-							wait(1.2)
+							wait(0.4)
 						end
 					end
 				end
@@ -126,7 +126,7 @@ game:GetService("UserInputService").InputBegan:Connect(function(R)
 				for _, v in ipairs(Players:GetPlayers()) do
 					if v ~= plr and v.Character and v.Character:FindFirstChild("HumanoidRootPart").Velocity.Magnitude <= 99 or  v ~= plr and v.Character and v.Character:FindFirstChildOfClass("Humanoid"):GetState() == Enum.HumanoidStateType.Running then
 						Fling(v.Name)
-						wait(1.2)
+						wait(0.4)
 					end
 				end
 			end)
