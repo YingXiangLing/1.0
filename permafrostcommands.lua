99d3134a051a25ed8de5d9ccbce53ce45de54d18
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-07-06 23:49:16 +0300

    Create permafrostcommands.lua

---
+++ permafrostcommands.lua
@@ -0,0 +1,10 @@
+--[[
+
+/permafrost:fakelock = fake robloxlocks workspace
+/permafrost:truelock = truly robloxlocks workspace
+/permafrost:fakedeath = uses fakedeath on workspace
+/permafrost:truedeath = uses truedeath on workspace
+/permafrost:abyss = basically abyss of death
+/permafrost:eternalfrost = all of these combined in 1 single attack
+
+]]--
