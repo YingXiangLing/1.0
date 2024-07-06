682589aef4546f4b19a6135cec06b2d16d175f88
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2024-07-06 23:52:58 +0300

    Create description.lua

---
+++ description.lua
@@ -0,0 +1,27 @@
+--[[
+
+i guess skids really like banning script builders???
+probably the least laggiest script builder
+-- rules --
+no server breakers
+do not execute bad scripts like neko
+------------------------------------------------
+----- features -----
+(i had to remove anti api ban since laggy)
+1. skids CANNOT change lighting and make really loud sounds
+2. this game has anti serverlock/ban so just rejoin like 5 times to get a server closed if it has serverlock or smt
+3. it encrypts everything you run so loggers dont work
+4. the gui has refit so it cannot be destroyed
+---- end ----
+
+---- commands ----
+/rm or /regenmap -> regenerate the map
+/r or /respawn -> respawns your character
+/rj or /rejoin -> rejoins the server
+/run (code) -> runs code trough chat if the gui is broken
+made by china user
+
+tags (ignore):
+script builder,void script builder,script,executor
+
+]]--
