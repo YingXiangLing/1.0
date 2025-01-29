d582e12dad12fb30e910302614445fdd54ef6494
YingXiangLing <164335474+YingXiangLing@users.noreply.github.com>
2025-01-29 15:42:03 +0200

    Delete permafrostcommands.lua

--- permafrostcommands.lua
+++
@@ -1,9 +0,0 @@
---[[
-
-/permafrost:fakelock = fake robloxlocks workspace
-/permafrost:truelock = truly robloxlocks workspace
-/permafrost:fakedeath = uses fakedeath on workspace
-/permafrost:truedeath = uses truedeath on workspace
-/permafrost:eternalfrost = all of these combined in 1 single attack
-
-]]--
